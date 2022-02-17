c90be773e696429773dd29571de46faaef67a07e
Еремин Олег <eremin_o@bsu.edu.ru>
2022-02-17 13:31:07 +0300

    Initial commit

---
+++ src/test/java/ru/edu/bsu/gosuslugi/GosuslugiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.edu.bsu.gosuslugi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosuslugiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
